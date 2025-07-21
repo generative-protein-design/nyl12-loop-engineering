dca4dae4bc96c4e88753c6c0ff7744a03f7d4ac7
Yakubov, Sergey <yakubovs@ornl.gov>
2025-07-21 11:10:14 -0400

    add a script to run boltz

---
+++ 2_run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+python prepare_boltz_input_nyl12.py \
+--input-dir output_test/1_ligandmpnn/seqs \
+--output-dir output_test/2_boltz/input \
+--cif-file /path/to/cif/file.cif
+
+
+/data/jpc/code/miniconda3/envs/boltz2.1.1/bin/boltz predict output_test/2_boltz/input \
+                             --model boltz2  \
+                             --output_format pdb \
+                             --use_msa_server \
+                             --msa_pairing_strategy greedy \
+                             --use_potentials \
+                             --cache /data/jpc/code/boltz
