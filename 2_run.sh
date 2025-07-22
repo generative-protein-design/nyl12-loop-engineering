c77d631aad875f023d4c79f5b9948bb31b1d4b95
Parks <parksjm@ornl.gov>
2025-07-22 11:51:55 -0400

    add cif template to 2_run.sh

--- 2_run.sh
+++ 2_run.sh
@@ -3,7 +3,7 @@
 python prepare_boltz_input_nyl12.py \
 --input-dir output_test/1_ligandmpnn/seqs \
 --output-dir output_test/2_boltz/input \
---cif-file /path/to/cif/file.cif
+--cif-file /data/jpc/code/heme_binder_diffusion/cif/Nyl12_refine13_no_tail.cif 
 
 
 /data/jpc/code/miniconda3/envs/boltz2.1.1/bin/boltz predict output_test/2_boltz/input \
